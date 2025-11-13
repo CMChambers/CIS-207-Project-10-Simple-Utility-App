fa136ffe542ab9fabd56ffeb1abaf42fd639d1fc
Chad M Chambers <61452826+CMChambers@users.noreply.github.com>
2025-11-13 13:18:16 -0500

    update/ fix formatting/ add greeting functionality

--- SimpleUtilityApp.cs
+++ SimpleUtilityApp.cs
@@ -19,7 +19,23 @@ namespace CIS207.Project10SimpleUtilityApp
         }
 
         private void GreetUser()
-        { Console.WriteLine("Hello User"); }
+        {
+            string name = GetInput.AsString("Enter your name");
+            Console.WriteLine($"{RandomGreeting()} {name}");
+        }
+
+        private string RandomGreeting()
+        {
+            Random rand = new Random();
+            int randomNum = rand.Next(0, 3);
+            switch (randomNum)
+            {
+                case 0: return "Hi";
+                case 1: return "Hello";
+                case 2: return "Hey";
+            }
+            return "What do you want";
+        }
 
         private void PrintMenu()
         {
@@ -27,7 +43,10 @@ namespace CIS207.Project10SimpleUtilityApp
             Console.WriteLine("1) Add Numbers");
             Console.WriteLine("2) Square Numbers");
             Console.WriteLine("3) Calculate a Discount");
-            menuSelection = GetInput.AsInt("Enter Selection: ", 0, 3);
+            Console.WriteLine("4) Even Check");
+            Console.WriteLine("5) Prime Check");
+
+            menuSelection = GetInput.AsInt("Enter Selection: ", 0, 5);
         }
 
         private void HandleMenuSelection()
@@ -46,16 +65,54 @@ namespace CIS207.Project10SimpleUtilityApp
                 case 3:
                     GoCalculateDiscount();
                     break;
+                case 4:
+                    GoEven();
+                    break;
+                case 5:
+                    GoPrime();
+                    break;
                 default:
                     break;
             }
         }
 
+        private void GoPrime()
+        {
+            int num = GetInput.AsInt("Enter the number");
+            Console.WriteLine($"{num} is {(IsPrime(num) ? "" : "not")} prime");
+        }
+
+        private bool IsPrime(int num)
+        {
+            if (num <= 1) { return false; }
+            if (num == 2) { return true; }
+            if (num % 2 == 0) { return false; }
+
+            int limit = (int)Math.Sqrt(num);
+
+            for (int i = 3; i <= limit; i += 2)
+            {
+                if (num % i == 0)
+                { return false; }
+            }
+
+            return true;
+        }
+
+        private void GoEven()
+        {
+            int num = GetInput.AsInt("Enter number");
+            Console.WriteLine($"{num} is {(IsEven(num) ? "even" : "odd")}");
+        }
+
+        bool IsEven(int num)
+        { return num % 2 == 0; }
+
         private void GoCalculateDiscount()
         {
-            double price = GetInput.AsDouble("Enter the price: ");
-            double discountRate = GetInput.AsDouble("Enter the discount rate: ");
-            Console.WriteLine($"${price} - {discountRate}% = ${CalculateDiscount(price, discountRate)}");
+            double price = GetInput.AsDouble("Enter the price");
+            double discountRate = GetInput.AsDouble("Enter the discount rate", 0, 100);
+            Console.WriteLine($"${price} - {discountRate}% = ${CalculateDiscount(price, discountRate):F2}");
         }
 
         private double CalculateDiscount(double price, double discountRate)
@@ -63,7 +120,7 @@ namespace CIS207.Project10SimpleUtilityApp
 
         private void GoSquareNumbers()
         {
-            int num = GetInput.AsInt("Enter the number to square: ");
+            int num = GetInput.AsInt("Enter the number to square");
             Console.WriteLine($"{num} squared = {SquareNumber(num)}");
         }
 
@@ -72,8 +129,8 @@ namespace CIS207.Project10SimpleUtilityApp
 
         private void GoAddNumbers()
         {
-            int num1 = GetInput.AsInt("Enter First Number: ");
-            int num2 = GetInput.AsInt("Enter Second Number: ");
+            int num1 = GetInput.AsInt("Enter First Number");
+            int num2 = GetInput.AsInt("Enter Second Number");
             AddNumbers(num1, num2);
         }
 
